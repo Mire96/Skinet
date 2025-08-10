7ae104b49eba1992814bc0adf12b1df72f02de72
Milos Mirkovic <milos.mirkovic@drillinginfo.com>
2025-08-10 12:26:28 +0200

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,36 +8,34 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController(IProductRepository repo) : ControllerBase
 {
-    private readonly StoreContext context;
-
-    public ProductsController(StoreContext context)
-    {
-        this.context = context;
-    }
+    
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        return await context.Products.ToListAsync();
+        return Ok(await repo.GetProductsAsync(brand, type, sort));
     }
 
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await context.Products.FindAsync(id);
+        var product = await repo.GetProductByIdAsync(id);
         return product != null ? product : NotFound();
     }
 
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        context.Products.Add(product);
-        await context.SaveChangesAsync();
-        // CreatedAtAction returns HTTP 201 Created status code with Location header
-        // pointing to the GetProduct endpoint, plus the product object in response body
-        // This follows REST API best practices instead of just returning the product
-        return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        repo.AddProduct(product);
+        if (await repo.SaveChangesAsync())
+        {
+            // CreatedAtAction returns HTTP 201 Created status code with Location header
+            // pointing to the GetProduct endpoint, plus the product object in response body
+            // This follows REST API best practices instead of just returning the product
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+        return BadRequest("Problem creating product");
     }
 
     [HttpPut("{id:int}")]
@@ -44,25 +43,42 @@ public class ProductsController : ControllerBase
     {
         if (id != product.Id || !ProductExists(id)) return BadRequest("Can not update product");
 
-        context.Entry(product).State = EntityState.Modified;
-        context.Products.Update(product);
-        await context.SaveChangesAsync();
-        return NoContent(); // 204 No Content
+        repo.UpdateProduct(product);
+        if(await repo.SaveChangesAsync())
+        {
+            return NoContent(); // 204 No Content
+        }
+        return BadRequest("Problem updating product");
     }
 
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteProduct(int id)
     {
-        var product = await context.Products.FindAsync(id);
+        var product = await repo.GetProductByIdAsync(id);
         if (product == null) return NotFound();
 
-        context.Products.Remove(product);
-        await context.SaveChangesAsync();
-        return NoContent(); // 204 No Content
+        repo.DeleteProduct(product);
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent(); // 204 No Content
+        }
+        return BadRequest("Problem deleting product");
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        return Ok(await repo.GetTypesAsync());
     }
 
     private bool ProductExists(int id)
     {
-        return context.Products.Any(e => e.Id == id);
+        return repo.ProductExists(id);
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,11 +11,31 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+//This step is used to seed the database with initial data
+//As well as Migrating any changes to the DB
+//So we don't have to do it manually through the terminal
+try
+{
+    //When you're outside of Dependency Injection, it is your responsibility to create the scope and dispose of it
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    //This way we can get any class we need 
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
 
 app.Run();
